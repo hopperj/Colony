8adc5d34389bd647e113e8462644fcbe1206b425
Jason Hopper <jason.t.hopper@gmail.com>
2014-11-09 15:38:29 -0400

    forgot new config

---
+++ Config.py
@@ -0,0 +1,25 @@
+#!/bin/python
+
+class Config:
+
+
+    def __init__(self):
+        title = "Scrolling test"
+        fps = 60
+        playtime = 0
+
+        xtiles = 1600
+        ytiles = 1600
+
+        width = 400
+        height = 400
+
+        bigmapwidth = 1600
+        bigmapheight = 1600
+
+        cornerpoint=[0,0]
+
+        scrollstepx = 5
+        scrollstepy = 5
+
+
